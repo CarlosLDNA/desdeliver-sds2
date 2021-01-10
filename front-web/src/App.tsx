b8f0392f6181ce039cf63958d978bc49bcc7caf9
CarlosLDNA <ca.gaspar@gmail.com>
2021-01-09 23:08:13 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
 import Navbar from './Navebar';
-import Home from './Home';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-        <Navbar />
-        <Home />
+        <Routes />
     </>
   );
 }
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import StepsHearder from './StepsHeader';
+
+function Orders() {
+  return (
+      <div className="orders-container">
+         <StepsHearder /> 
+      </div>
+  )
+}
+
+export default Orders;
